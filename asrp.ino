a429e4f9acc9c204d55c128b045f22d44534d76a
Charlemagne44 <collinhay44@gmail.com>
2023-08-02 09:54:48 -0400

    clear out main testing, don't push until final

--- asrp.ino
+++ asrp.ino
@@ -1,22 +1,13 @@
 #include "PhotoResistor.h"
 
-#define LOOP_INTERVAL 30000
+#define LOOP_INTERVAL 3000
 
-PhotoResistor leftPhotoResistor(7);
-PhotoResistor rightPhotoResistor(8);
+PhotoResistor photoResistor(A0);
 
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
-    // Gather the intensity detected from the photo resistors on a scale of 1-5
-    int leftIntensity = leftPhotoResistor.getLightIntensity();
-    int rightIntensity = leftPhotoResistor.getLightIntensity();
-    Serial.print("left photoresistor intensity: ");
-    Serial.println(leftIntensity);
-    Serial.print("Right photoresistor intensity: ");
-    Serial.println(rightIntensity);
-    
     delay(LOOP_INTERVAL);
 }
